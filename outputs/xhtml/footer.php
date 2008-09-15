068be6fbb9704ca14cb241e7268bc24ffc5fdf33
Kaity G. B <uberChicGeekChick@uberChicGeekChick.Com>
2008-09-14 20:04:50 -0600

    I'm still having CSS issues with my site's link to the RPL & my git repo.  So for now I'm just keeping the XHTML hiddan.

--- outputs/xhtml/footer.php
+++ outputs/xhtml/footer.php
@@ -1,11 +1,13 @@
 <?php
 	print <<<BLOC
+		<!--
 		<br clear='all'/>
 		<div class='copyright'>
 			This site is powered by <a href='http://github.com/uberchicgeekchick/speaking-out/tree'>speaking-out</a><br/>
 			Both are &copy; 2008 Kaity G. B.<br/>
 			realFriends is released the <a href='http://opensource.org/licenses/rpl1.5.txt'>Reciprocal Public License(RPL) 1.5</a>
 		</div>
+		-->
 	</body>
 </html>
 BLOC
