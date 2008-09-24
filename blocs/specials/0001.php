2765128b121bdcb368acac18ee384141aa2b104c
Kaity G. B <uberChicGeekChick@uberChicGeekChick.Com>
2008-09-23 20:24:23 -0600

    Skipped several files on my last commit . . . but its all for the same changes.

---
+++ blocs/specials/0001.php
@@ -0,0 +1,19 @@
+<?php
+	/*
+	 * Unless explicitly acquired and licensed from Licensor under another
+	 * license, the contents of this file are subject to the Reciprocal Public
+	 * License ("RPL") Version 1.5, or subsequent versions as allowed by the RPL,
+	 * and You may not copy or use this file in either source code or executable
+	 * form, except in compliance with the terms and conditions of the RPL.
+	 *
+	 * All software distributed under the RPL is provided strictly on an "AS
+	 * IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND
+	 * LICENSOR HEREBY DISCLAIMS ALL SUCH WARRANTIES, INCLUDING WITHOUT
+	 * LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+	 * PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific
+	 * language governing rights and limitations under the RPL.
+	 */
+	print <<<BLOC
+		The Cranks talk about Project Kenai, Microsoft Ad, Bill Gates, Jerry Seinfeld, Vegemite, Ohio Linux Fest, Food, and many other random topics. Also the aftershow is included for your listening pleasure. Cranks this week: monsterb, 330, Peter64, klaatu, jlindsay, Azimuth, Xoke, Jack Johnson, Barry Williams, UberChick, and Timrit.		
+BLOC;
+?>
