e24a1751666c1d5b8f2ef0692ce295ac8d8fe050
Kaity G. B <uberChick@uberChicGeekChick.Com>
2008-10-09 03:34:49 -0600

    Fixing a lil display issue in the way new lines were wrapping my links, copyright, & OSS links.

--- outputs/xhtml/footer.php
+++ outputs/xhtml/footer.php
@@ -2,9 +2,10 @@
 	print <<<BLOC
 		<div class='right_sides_blocs'>
 			<div class='copyright'>
-				powered by <a href='http://github.com/uberchicgeekchick/speaking-out/tree'>speaking-out</a><br/>
-				&copy; 2008 Kaity G. B.<br/>
-				<a href='http://github.com/uberchicgeekchick/speaking-out/tree'>speaking-out</a> is <a href='http://opensource.org/' title='Free, as in freedom, &amp; open source software,'>F&amp;OSS</a><br/>released under the <a href='http://opensource.org/licenses/rpl1.5.txt' title='Reciprocal Public License'>RPL 1.5</a>
+				<nobr>my site &amp; podcast are</nobr><br/>
+				<nobr>&copy; 2008 Kaity G. B.</nobr><br/>
+				<nobr>powered by <a href='http://github.com/uberchicgeekchick/speaking-out/tree'>speaking-OUT</a></nobr><br/>
+				<nobr><a href='http://github.com/uberchicgeekchick/speaking-out/tree'>speaking-OUT</a> is <a href='http://opensource.org/' title='Free, as in freedom, &amp; open source software,'>F&amp;OSS</a><br/>released under the <a href='http://opensource.org/licenses/rpl1.5.txt' title='Reciprocal Public License'>RPL 1.5</a></nobr>
 			</div>
 			<br/>
 BLOC;
