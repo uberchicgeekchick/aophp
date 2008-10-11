019c2fc9af9a1a2dc7f0c4fc9fa8a3997134c6a4
Kaity G. B <uberChick@uberChicGeekChick.Com>
2008-10-11 05:07:42 -0600

    Implamenting 'events' feature.  The first 'event' is Breast Cancer awareness Podcastathon hosted by GeekIsChic.Org

--- blocs/specials/0002.php
+++ blocs/specials/0002.php
@@ -2,11 +2,24 @@
 print <<<SPECIAL
 		<h1>Expressive Special</h1>
 		<h2>Geek Is Chic&#039;s 24hr Podcastathon</h2>
+			Its not to late: <a href='http://www.new.facebook.com/event.php?eid=27355222103'><em><strong>RSVP!</strong></em></a> &amp; remember to show up &amp; help us raise awareness, support, &amp; donations to find a cure!
 		<p align='center'>
-			<img src='./graphics/banners/GeekIsChic.png' width='474' height='318' border='0'/>
+			<a href='http://geekischic.org/'>Join your sister nerd &amp; geek chicks as we work together to fight!<br/>
+			keep all of our boobs: safe &amp; sound! keep our girls round &amp; around!<br/>
+			<a href='http://GeekIsChic.Org/'><img src='./graphics/banners/GeekIsChic.png' width='474' height='318' border='0'/><a/><br/>
+			<a href='http://GeekIsChic.Org/'>http://GeekIsChic.Org/</a><br/>
 		</p><p>
-			<a href='http://www.flickr.com/groups/geekischic/'>flickr banners - where you ribbon::change your avatar</a>
-			<a href='http://www.new.facebook.com/event.php?eid=27355222103'>RSVP to show your support, than show up to help us raise awareness, support, &amp; donations to help us find a cure.</a>
+			<ul>
+				<lh>Join us in showing you support for a cure!</lh>
+				<li>Visit us online at: <a href='http://www.talkshoe.com/tc/1256/'>http://www.talkshoe.com/tc/12560</a><li/>
+				<li>There are two way to call in to share your story &amp; show your support:</li>
+				<ul>
+					<li>Using your favorite SIP client, like the free <a href='http://Gizmo5.Com/'>Gizmo5</a>, call <a href='sip:GeekIsChic-12560@66.212.134.192'>sip:GeekIsChic-12560@66.212.134.192</a>></li>
+					<li>Or via mobile or telephone dial: <em>1-727-444-7444</em></li>
+				</ul>
+				<li>When asked to enter the <em>Call ID</em> enter <strong>12560</strong> and help us come even a lil closer to a cure!</li>
+			</ul>
+			<a href='http://www.flickr.com/groups/geekischic/'>flickr banners - where you ribbon::change your avatar</a><br/>
 		</p>
 SPECIAL;
 ?>
