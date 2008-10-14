979c16984dc9afd0d9b5d2f0e784d3935d15d1c2
Kaity G. B <uberChick@uberChicGeekChick.Com>
2008-10-14 10:22:04 -0600

    A lil update since the podcastathon is over.

--- blocs/specials/0002.php
+++ blocs/specials/0002.php
@@ -2,24 +2,16 @@
 print <<<SPECIAL
 		<h1>Expressive Special</h1>
 		<h2>Geek Is Chic&#039;s 24hr Podcastathon</h2>
-			Its not to late: <a href='http://www.new.facebook.com/event.php?eid=27355222103'><em><strong>RSVP!</strong></em></a> &amp; remember to show up &amp; help us raise awareness, support, &amp; donations to find a cure!
 		<p align='center'>
+			In 24 hours $525 was raised all for the <a href='http://www.komen.org/'>Susan G. Koman Foundation</a><br/>
 			<a href='http://geekischic.org/'>Join your sister nerd &amp; geek chicks as we work together to fight!<br/>
 			keep all of our boobs: safe &amp; sound! keep our girls round &amp; around!<br/>
 			<a href='http://GeekIsChic.Org/'><img src='./graphics/banners/GeekIsChic.png' width='474' height='318' border='0'/><a/><br/>
 			<a href='http://GeekIsChic.Org/'>http://GeekIsChic.Org/</a><br/>
 		</p><p>
 			<ul>
-				<lh>Join us in showing you support for a cure!</lh>
-				<li>Watch &amp; chat on <a href='http://uStream.tv/iandrea'>uStream.tv/iandrea</a></li>
-				<li>Or join us on IRC @ <a href='irc://irc.freenode.net/Podcastathon'>irc://irc.freenode.net/Podcastathon</a></li>
-				<li>Call in, listen in, be a part of finding a cure! @ <a href='http://www.talkshoe.com/tc/1256/'>http://www.talkshoe.com/tc/12560</a></li>
-				<li>There are two way to call in to share your story &amp; show your support:</li>
-				<ul>
-					<li>Using your favorite SIP client, like the free <a href='http://Gizmo5.Com/'>Gizmo5</a>, call <a href='sip:GeekIsChic-12560@66.212.134.192'>sip:GeekIsChic-12560@66.212.134.192</a>></li>
-					<li>Or via mobile or telephone dial: <em>1-727-444-7444</em></li>
-				</ul>
-				<li>When asked to enter the <em>Call ID</em> enter <strong>12560</strong> and help us come even a lil closer to a cure!</li>
+				<lh>The podcastathon is over, but you can still help out donations are being accepted until October 17th!</lh>
+				<li>If you have questions need help in donating I&#039;m still on IRC @ <a href='irc://irc.freenode.net/Podcastathon'>irc://irc.freenode.net/Podcastathon</a></li>
 			</ul>
 			<a href='http://www.flickr.com/groups/geekischic/'>flickr banners - where you ribbon::change your avatar</a><br/>
 			To busy? No worries, just please help &amp; donate. All donations are to the Susan G. Komen Foundation<br/>
