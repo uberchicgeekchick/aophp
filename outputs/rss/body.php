d173c994537dbd5b21114314a2d3f7ecfb3a6d3a
Kaity G. B <uberChick@uberChicGeekChick.Com>
2008-10-14 10:52:48 -0600

    Adding content from the GeekIsChic podcastathon.

--- outputs/rss/body.php
+++ outputs/rss/body.php
@@ -1,5 +1,5 @@
-<?xml version="1.0" encoding="utf-8"?>
-<rss version="2.0">
+<?xml version='1.0' encoding='utf-8'?>
+<rss version='2.0'>
 	<channel>
 		<title>Expressive Programming</title>
 		<description>
@@ -7,13 +7,24 @@
 		</description>
 		<link>http://uberChicGeekChick.Com/</link>
 		<language>en-US</language>
-		
+		<item>
+			<title>GeekIsChic - Podcastathon 2008 Highlights - Melissa Thiessen</title>
+			<description>GeekIsChic - Podcastathon 2008 Highlights We're joined by Melissa Thiessen of Girls Gone Geek.  You can still donate until 10/17/2008 @ http://www.geekischic.org/</description>
+			<enclosure url='http://recordings.talkshoe.com/TC-12560/TS-155843.mp3' length='19163353' type='audio/mpeg'/>
+			<guid>http://recordings.talkshoe.com/TC-12560/TS-155843.mp3</guid>
+			<pubDate>2008-10-14</pubDate>
+		</item><item>
+			<title>GeekIsChic - Podcastathon 2008 Highlights - Stephanie Ravenscraft</title>
+			<description><![CDATA[GeekIsChic - Podcastathon 2008 Highlights:  We're joined by Stephanie Ravenscraft of <a href='http://GSPN.tv/>GSPN.tv</a><br/>You can still donate until 10/17/2008 @ <a href='http://www.geekischic.org/'>GeekIsChic.Org</a>]]></description>
+			<enclosure url='http://recordings.talkshoe.com/TC-12560/TS-155846.mp3' length='19163353' type='audio/mpeg'/>
+			<guid>http://recordings.talkshoe.com/TC-12560/TS-155846.mp3</guid>
+			<pubDate>2008-10-14</pubDate>
 		<item>
 			<title>Expressive Programming::0002::Producing Perfection?</title>
 			<link>http://uberChicGeekChick.Com/episodes/0002</link>
-			<description><![CDATA[Episode 0002::Producing Perfection?]]></description>
+			<description><![CDATA[Episode 0002::Producing Perfection? Its not possible!  Why do we all still hold that as our goal.&nbsp; How high are your standards for yourself?]]></description>
 			<pubDate>2008-09-18</pubDate>
-			<enclosure url="http://hackerpublicradio.org/eps/hpr0188.mp3" length="19163353" type="audio/mpeg"/>
+			<enclosure url='http://hackerpublicradio.org/eps/hpr0188.mp3' length='19163353' type='audio/mpeg'/>
 			<guid>http://hackerpublicradio.org/eps/hpr0188.mp3</guid>
 		</item>
 		
@@ -22,7 +33,7 @@
 			<link>http://uberChicGeekChick.Com/specials/0001</link>
 			<description><![CDATA[My &quot;first time&quot; on the LinuxCranks Show.]]></description>
 			<pubDate>2008-09-15</pubDate>
-			<enclosure url="http://feeds.feedburner.com/~r/LinuxCranks/~5/392913292/LinuxCranks3x11.ogg" length="59615778" type="application/ogg" />
+			<enclosure url='http://feeds.feedburner.com/~r/LinuxCranks/~5/392913292/LinuxCranks3x11.ogg' length='59615778' type='application/ogg' />
 			<guid>http://feeds.feedburner.com/~r/LinuxCranks/~5/392913292/LinuxCranks3x11.ogg</guid>
 		</item>
 		
@@ -31,7 +42,7 @@
 			<link>http://uberChicGeekChick.Com/episodes/0001</link>
 			<description><![CDATA[Expressive Programming&#039; debut: Welcome to the exhibit!  episode 0001]]></description>
 			<pubDate>2008-08-18</pubDate>
-			<enclosure url="http://hackerpublicradio.org/eps/hpr0165.mp3" length="16419367" type="audio/mpeg"/>
+			<enclosure url='http://hackerpublicradio.org/eps/hpr0165.mp3' length='16419367' type='audio/mpeg'/>
 			<guid>http://hackerpublicradio.org/eps/hpr0165.mp3</guid>
 		</item>
 	</channel>
