b926da270ee89e3be7ad2066fd9942784785d0bf
Kaity G. B <uberChick@uberChicGeekChick.Com>
2008-10-24 21:30:41 -0600

    I updated my menu to show my episodes and added another link to episode 3.

--- blocs/core/menu.php
+++ blocs/core/menu.php
@@ -1,7 +1,8 @@
 <?php
 		print <<<MENU
 		<div class='menu'>
-			<a class='menu_link' href='./'>Home</a>
+			<a class='menu_link' href='./'>Home</a><br/>
+			episodes: | <a class='menu_link' href='./?episodes=0003'>0003: Who, what, where, when, &amp; why?</a> | <a class='menu_link' href='./?episodes=0002'>0002: Producing Perfection?</a> | <a class='menu_link' href='./?episodes=0001'>0001: Welcome to the exhibit!</a> |<br/>
 		</div>
 MENU;
 ?>
--- blocs/episodes/0003.php
+++ blocs/episodes/0003.php
@@ -1,7 +1,7 @@
 <?php
 				print <<<BLOC
 				<h1>Expessive Programming</h1>
-				<h2>Episode 0003: Who, what, where, when, why?</h2>
+				<h2>Episode 0003: Who, what, where, when, &amp; why?</h2>
 				[<a href="http://HackerPublicRadio.Org/eps.php?id=208">download this episode&#039;s mp3 from HPR</a>]
 				<p>
 					Now matter what you program motivation comes into play.  But what motivates you?
@@ -10,6 +10,7 @@
 				</p>
 				<ul>
 					<lh>Links mentioned in this episode:</lh>
+					<li><a href='http://www.raydium.org/'>Raydium: 3-D Game Engine</a></li>
 					<li><a href='http://www.crystalspace3d.erg/'>Crystal Space 3-D</a></li>
 					<li><a href='http://www.opengl.org/'>openGL</a></li>
 					<li><a href='http://openscenegraph.org/'>OpenSceneGraph</a></li>
