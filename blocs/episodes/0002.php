2ac353f3e0bccf0b60c63e33dc51e5aeffb44e8a
Kaity G. B <uberChick@uberChicGeekChick.Com>
2008-10-27 03:23:57 -0600

    I added links to my uploads to Internet Archive & my contributions to Hacker Public Radio. I will be adding download links to each episodes page so that enclosures can be pulled via xpath or, more hopefully, I will be able to import blocs into MySQL before episode 4.  Though now that I am refocusing on making my game work on this will be slow.  Since I will need a web app to push my game development on this and all my other projects will prolly increase.  Once I can get at least AOPHP to use a StorageEngine layer, 1st MySQL, development on my web app platform will increase a lot.  My primary focus is on my creating my game.

--- blocs/episodes/0002.php
+++ blocs/episodes/0002.php
@@ -1,8 +1,8 @@
 <?php
 				print <<<BLOC
 				<h1>Expessive Programming</h1>
-				<h2>Episode 0002: Producing Perfection?</h2>[<a href="http://www.archive.org/details/ExpressiveProgramming-0002-ByUberchick">this episode&#039;s Internet Archive page</a>]<br/>
-				[<a href="http://HackerPublicRadio.Org/eps.php?id=188">this episode&#039;s page on HPR.</a>]<br/>
+				<h2>Episode 0002: Producing Perfection?</h2>[<a href="http://www.archive.org/details/ExpressiveProgramming-0002-ByUberchick">this episode&#039;s Internet Archive page.</a>]<br/>
+				[<a href="http://HackerPublicRadio.Org/eps.php?id=188">this episode&#039;s HPR page.</a>]<br/>
 				<p>
 					Welcome to my website &amp; the homepage for my podcast: `Epressive Programming`.&nbsp; `Epressive Programming` is a <a href="http://HackerPublicRadio.Org/">HackerPublicRadio</a> series &amp; produced w/a huge thnx to Enigma.&nbsp; All of the music featured in `ExpressiveProgramming` is by the band Hot.Bitch.Arsenal; their website is <a href="http://HotBitchArsenal.Com/">http://HotBitchArsenal.Com/</a>.
 				</p><p>
--- blocs/episodes/0003.php
+++ blocs/episodes/0003.php
@@ -3,7 +3,7 @@
 				<h1>Expessive Programming</h1>
 				<h2>Episode 0003: Who, what, where, when, why?</h2>
 				[<a href="http://www.archive.org/details/ExpressiveProgramming-0003-ByUberchick">this episode&#039;s Internet Archive page</a>]<br/>
-				[<a href="http://HackerPublicRadio.Org/eps.php?id=208">this episode&#039;s page on HPR</a>]<br/>
+				[<a href="http://HackerPublicRadio.Org/eps.php?id=208">this episode&#039;s HPR page</a>]<br/>
 				<p>
 					Now matter what you program, or why, motivation comes into play.&nbsp; But what motivates you?&nbsp; Is it what you'll accomplish once you're done; is it programming itself; is it the benifit, or effect, that your program will bring to others?&nbsp; Is it the money you make or is it something more.&nbsp; Can what we do make our world better?&nbsp; I feel it can on an idividual &amp; global scales.
 				</p><p>
@@ -15,7 +15,7 @@
 					<li><a href='http://www.crystalspace3d.erg/'>Crystal Space 3-D</a></li>
 					<li><a href='http://www.opengl.org/'>openGL</a></li>
 					<li><a href='http://openscenegraph.org/'>OpenSceneGraph</a></li>
-					<li><a href='http:///'></a></li>
+					<li><a href='http://blender.org/'>Blender</a></li>
 					<li><a href='http:///'></a></li>
 				</ul>
 BLOC;
