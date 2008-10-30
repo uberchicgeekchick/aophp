156bbbb21e1f405a00f89d8b8f425da65db37389
Kaity G. B <uberChick@uberChicGeekChick.Com>
2008-10-30 04:32:25 -0600

    I updated my theme and have added links directly to my github repos.

--- blocs/formats/XHTML/header.php
+++ blocs/formats/XHTML/header.php
@@ -8,19 +8,20 @@
 <?xml version='1.0' encoding='UTF-8'?>
 <html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>
 	<head>
-		<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+		<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
 		<title>uberChicGeekChick - programming, art, creativity, code, self expression, &amp; women(BTW: WE ROCK!) </title>
+
+		<link rel='shortcut icon' href='./graphics/favicon.png'/>
+
 		<!--
-		<meta name='description' value='{About my page}' />
-		<meta name='keywords' content='{My page&#39;s template., keywords, about, this, page.}' />
+		<meta name='description' value='{About my page}'/>
+		<meta name='keywords' content='{My page&#39;s template., keywords, about, this, page.}'/>
 		
-		<link rel='shortcut icon' href='./favicon.png' />
+		<link rel='alternate' type='application/rss+xml' title='RSS 2.0 Feed' href='./rss.xml'/>
+		<link rel='alternate' type='application/atom+xml' title='Atom 0.3 Feed' href='./atom.xml'/>
 		
-		<link rel='alternate' type='application/rss+xml' title='RSS 2.0 Feed' href='./rss.xml' />
-		<link rel='alternate' type='application/atom+xml' title='Atom 0.3 Feed' href='./atom.xml' />
-		
-		<link rel='pingback' type='application/pingback+xml' href='./xmlrpc.php' />
-		<link rel='EditURI' type='application/rsd+xml' title='RSD' href='./xmlrpc.php/rsd />
+		<link rel='pingback' type='application/pingback+xml' href='./xmlrpc.php'/>
+		<link rel='EditURI' type='application/rsd+xml' title='RSD' href='./xmlrpc.php/rsd/>
 		<script language='javascript' src='./javascript.json.js'></script>
 		-->
 		<!-- NOTE: Firebug can't debug @import rules. -->
