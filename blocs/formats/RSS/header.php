956b1c6bb30e162df12dcde947b86b69a4e40bc6
Kaity G. B <uberChick@uberChicGeekChick.Com>
2008-11-17 12:21:17 -0700

    I have released 'Expressive Programming', episode 0004: 'Escapism and Resources' and its HackerPublicRadio information too.

--- blocs/formats/RSS/header.php
+++ blocs/formats/RSS/header.php
@@ -8,7 +8,7 @@
 <rss version='2.0'>
 	<channel>
 		<title>Expressive Programming</title>
-		<description>tt
+		<description>
 			<![CDATA[uberChicGeekChick: Expressive Programming, the podcast &amp; more, about: programming, art, creativity, code, self expression, &amp; uber women (WE ROCK!).&nbsp; Programming is an art form, a creative outlet, &amp; most powerfully it can also be on outlet for our self expression.&nbsp; Here I look at programming as self expression. art, &amp; other ways that programming improves &amp; reflects our life, our self, &amp; our desire to improve our life.]]>
 		</description>
 		<link>http://uberChicGeekChick.Com/</link>
--- blocs/formats/RSS/hpr.rss.php
+++ blocs/formats/RSS/hpr.rss.php
@@ -1,5 +1,14 @@
 		
 		<item>
+			<title>Expressive Programming::Episode 0004::Escapism and Resources.</title>
+			<link>http://hackerpublicradio.org/eps.php?id=230</link>
+			<description><![CDATA[<h1>Expressive Programming</h1><h2>Episode 0004::Escapism and Resources.</h2><p>Is programming, & possibly all creative projects, be used as escapism? For me it was what got me into computer programming. Now today as I try to live with a neuro-muscular disease called DYT+1 Early-Onset Generalized Dystonia I find programming to not only be my truest form of art & self expression. But its also an invaluable way for me to escape from the torment caused by my disease.</p><p>I also talk about by further work on my new 3-D game project. I talk about the engine I've chosen & what work I've done so far. Mostly I discuss managing 'non-code resources' of any large project. I especially focus on my game's 3-D models & textures.</p><ul><lh>Links mentioned in this episode:</lh><li><a href='http://www.raydium.org/'>Raydium: 3-D Game Engine</a></li><li><a href='http://blender.org/'>Blender</a></li></ul>]]></description>
+			<pubDate>2008-11-17</pubDate>
+			<enclosure url='http://hackerpublicradio.org/eps/hpr0230.mp3' length='18804666' type='audio/mpeg'/>
+			<guid>http://hackerpublicradio.org/eps/hpr0230.mp3</guid>
+		</item>
+		
+		<item>
 			<title>Expressive Programming::0003::Who, what, where, when, why.</title>
 			<link>http://hackerpublicradio.org/eps.php?id=208</link>
 			<description><![CDATA[Episode 0003::Now matter what you program motivation comes into play.  But what motivates you?]]></description>
