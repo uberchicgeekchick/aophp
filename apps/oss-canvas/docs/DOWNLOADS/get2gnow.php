604b16e82b4fd7a97353fd9e1a0b765fb82cca06
uberChick <uberChick@uberChicGeekChick.Com>
2009-06-19 01:19:59 -0600

    AOPHP 000000aFD - project updates.
    One of my final updates before AOPHP is recreated as an AO accessed REST resourses.
    
    I'm getting ready to release get2gnow, my next version of connectED, & AOPHP will be soon completely recreated.  I've also added screenshot suppotr to oss-canvase & add 2-3 screen shots for get2gnow.  I bunch of improvements to get2gnow's revamped tweet_list have been made today & so I've had to point to my most robust git commit.  Included in my improvements is a foreach function that keeps new tweets sorting correctly.  I've also further improved get2gnow's tweet-list interface so all of the 'text' columns can be resized & they will dynamically wrap correctly.  Its so nice to have all of get2gnow now using GtkBuilder for all UI.
    
    Now with some hope & luck I can truly start to create my ideal microblogging client.  I just had to implement a gtk based error, no biggie...now beddie-bye.
    
    Committed on: 2009-06-19 @ 01:19:59 AM

--- apps/oss-canvas/docs/DOWNLOADS/get2gnow.php
+++ apps/oss-canvas/docs/DOWNLOADS/get2gnow.php
@@ -47,7 +47,7 @@
 	 *
 	 */
 	$versions=array(
-		array("#0000F6aEC", "http://github.com/uberchicgeekchick/get2gnow/tree/4304be5cd029ae80aacba741966214edcbfbc8c1"),
+		array("#0000F6aEC", "http://github.com/uberchicgeekchick/get2gnow/tree/657ce1b94df1789f4efe9974d1851962e068d941"),
 		array("#0000DDaC4", "http://github.com/uberchicgeekchick/get2gnow/tree/2153cc59673836859000652727bd78e99f5f5806"),
 		array('00.00.DB.a09', "http://github.com/uberchicgeekchick/get2gnow/tree/9c8cf017e8fafc9ea8945ed9a9d582fa922425c4"),
 		array('00.00.DB.aFF', "http://github.com/uberchicgeekchick/get2gnow/tree/4b789ae4d47f77cc786d7f0e7ebdc7a1d6760cc8"),
