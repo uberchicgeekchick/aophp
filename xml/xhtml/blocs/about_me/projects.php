5265fe74b839bca2b42ea878ed77269c091317c7
uberChick <uberChick@uberChicGeekChick.Com>
2009-06-20 00:54:54 -0600

    AOPHP 000000aFD - project updates.
    One of my final updates before AOPHP is recreated as an AO accessed REST resourses.
    
    I'm getting ready to release get2gnow, my next version of connectED, & AOPHP will be soon completely recreated.  I've also added screenshot suppotr to oss-canvase & add 2-3 screen shots for get2gnow.  I bunch of improvements to get2gnow's revamped tweet_list have been made today & so I've had to point to my most robust git commit.  Included in my improvements is a foreach function that keeps new tweets sorting correctly.  I've also further improved get2gnow's tweet-list interface so all of the 'text' columns can be resized & they will dynamically wrap correctly.  Its so nice to have all of get2gnow now using GtkBuilder for all UI.
    
    Now with some hope & luck I can truly start to create my ideal microblogging client.  I just had to implement a gtk based error, no biggie...now beddie-bye.
    
    Committed on: 2009-06-20 @ 12:54:54 AM

--- xml/xhtml/blocs/about_me/projects.php
+++ xml/xhtml/blocs/about_me/projects.php
@@ -40,7 +40,7 @@
 							<li class='my_projects'><a href='./?projects=get2gnow'>get2gnow</a></li>
 							<ul class='github_releases'>
 								<li class='github_repo'><a href='https://www.github.com/uberchicgeekchick/get2gnow/tree/master'>master repo</li>
-								<li class="github_release"><span style="color:#999999; background-color:#0000F6aEC;"><a href="http://github.com/uberchicgeekchick/get2gnow/tree/74eee7ef76ae8db5ed7fa3ecdfef7cf0badc1897">release #0000F6aEC</a></span></li>
+								<li class="github_release"><span style="color:#999999; background-color:#0000F6aEC;"><a href="http://github.com/uberchicgeekchick/get2gnow/tree/8d3aa659d0c1f0e108b7f29ee743dc7c605ac1cd">release #0000F6aEC</a></span></li>
 								<ul class='my_projects'>
 									<lh>ScreenShots</lh>
 									<li class='my_projects'><a href='./apps/oss-canvas/graphics/get2gnow/get2gnow.png'>ScreenShot #1</a></li>
